1e1174eee4e5cd59f03bd485ea48068692088f8e
Ahmad <Ahmadmuhammadmak5@gmail.com>
2020-08-11 02:42:56 +0100

    updated posts

--- frontend/src/components/post/PostList.jsx
+++ frontend/src/components/post/PostList.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PostData from './PostData';
+import Typography from '@material-ui/core/Typography';
 
 export default function PostList() {
   const posts = useSelector((state) => state.posts);
   console.log(posts);
   return (
     <React.Fragment>
-      {posts.loading
-        ? 'Loading posts wait ...'
-        : posts.posts.map((post) => <PostData post={post} key={post._id} />)}
+      {posts.loading ? (
+        <Typography variant='body1' color='initial'>
+          Loading posts wait ...
+        </Typography>
+      ) : (
+        posts.posts.map((post) => <PostData post={post} key={post._id} />)
+      )}
+      {!posts.length && (
+        <Typography variant='body1' color='initial'>
+          No post found
+        </Typography>
+      )}
     </React.Fragment>
   );
 }
